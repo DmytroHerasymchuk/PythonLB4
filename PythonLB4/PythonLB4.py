394932f993f1d8f40733221dd91c8c0c13f7a862
DmitriyGG03 <dimchikdg89@gmail.com>
2023-05-09 01:25:30 +0300

    Add class and main function. Made code review

--- PythonLB4/PythonLB4.py
+++ PythonLB4/PythonLB4.py
@@ -1,29 +1,44 @@
 import os
 import requests
-import math
+import msvcrt
+import sys
 
-url = "https://api.coincap.io/v2/assets?limit=10"
+class url_currencies_getter:
+    urlBase = "https://api.coincap.io/v2/assets?limit="
 
-def getCurrencies(number_of_currencies):
-    url = f"https://api.coincap.io/v2/assets?limit={number_of_currencies}"
-    response = requests.get(url);
-    return response.json()["data"]
+    def start(self):
+        while(True):
+            os.system('cls')
 
-next_page = True
-while(next_page):
-    print("Enter a number of currencies you want to see")
-    user_input = input()
-    if(user_input.isdigit() and int(user_input) > 0):
-        os.system('cls')
-        list_of_currencies = getCurrencies(user_input)
-        for currency in list_of_currencies:
-            name = currency["name"]
-            symbol = currency["symbol"]
-            price = round(float(currency["priceUsd"]),5)
-            print(f"{name} {symbol} \n\t Price = {price} $\n")
-    else:
-        print("Enter a number!")
-    
+            inputValue = input("Enter a number of currencies you want to see: ")
+
+            if(inputValue == "exit"): sys.exit()
+
+            if(inputValue.isdigit() and int(inputValue) > 0):
+                os.system('cls')
+                list_of_currencies = self.getCurrencies(inputValue)
+
+                for currency in list_of_currencies:
+                    name = currency["name"]
+                    symbol = currency["symbol"]
+                    price = round(float(currency["priceUsd"]),5)
+                    print(f"{name} {symbol} \n\t Price = {price} $\n")
+            else:
+                print("You must enter a number!")
+            
+            print("\nEnter any button")
+            msvcrt.getch()
+
+    def getCurrencies(self, number_of_currencies):
+        url = f"{self.urlBase}{number_of_currencies}"
+        response = requests.get(url);
+        return response.json()["data"]
+
+def main():
+    start_class = url_currencies_getter()
+    start_class.start()
+
+main()
 
     
     
